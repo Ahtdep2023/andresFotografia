6fc953eba70635b94754664b116b1198cec5a92f
toleAndresDep <andres__toledo@hotmail.com>
2023-07-06 13:00:30 -0300

    cambio de iconos de salida de fotos

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* esto sirve para achicar el alto de la navbar*/
-const nav = document.querySelector(".navegador")
-window.addEventListener("scroll", fixNav)
+// const nav = document.querySelector(".navegador")
+// window.addEventListener("scroll", fixNav)
 
-function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 100) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
-}
+// function fixNav() {
+//     if(window.scrollY > nav.offsetHeight + 100) {
+//         nav.classList.add('active')
+//     } else {
+//         nav.classList.remove('active')
+//     }
+// }
